b51abe98c630f6315a68105501a94eb85017e950
LoganH-R <146144771+LoganH-R@users.noreply.github.com>
2023-10-24 23:03:47 +0000

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get("/rock", {:controller => "game", :action => "user_played_rock" })
 
+  get("/paper", {:controller => "game", :action => "user_played_paper" })
+
+  get("/scissors", {:controller => "game", :action => "user_played_scissors" })
+
+  get("/", {:controller => "game", :action => "rules"})
 end
+# z = ZebraController.new   this is what rails does behind the scenes when you do ^
+# z.giraffe   method giraffe
+# rails looks for this in app/controllers
